517f9e25d6eabfdfa3b1d917cdacd05682a9a168
Stefan Lieser <stefan@lieser-online.de>
2015-08-07 16:55:21 +0200

    Branching data flow added

---
+++ source/csharp/implementation/implementation/integration/BranchingDataFlow.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace implementation
+{
+	public class Checkout_Processor
+	{
+		public void Checkout(Customer customer) {
+			If_Customer_has_credit(customer,
+				Checkout_with_credit_card,
+				Checkout_with_cash);
+		}
+
+		public void If_Customer_has_credit(Customer customer, Action has_credit, Action has_no_credit) {
+			if(customer.Balance >= 1000.0) {
+				has_credit();
+			} else {
+				has_no_credit();
+			}
+		}
+
+		public void Checkout_with_credit_card() {
+			// ...
+		}
+
+		public void Checkout_with_cash() {
+			// ...
+		}
+	}
+
+	public class Customer
+	{
+		public string Name { get; set; }
+
+		public double Balance { get; set; }
+
+		// ...
+	}
+}
+
