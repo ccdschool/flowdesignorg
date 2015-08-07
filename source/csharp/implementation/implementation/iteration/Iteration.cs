001f2e954f90b460ebe2b08b581698f2c1c84c30
Stefan Lieser <stefan@lieser-online.de>
2015-08-07 17:26:34 +0200

    Adding diagram and implementation for iteration flow

---
+++ source/csharp/implementation/implementation/iteration/Iteration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace implementation
+{
+	public class MyClass
+	{
+		public IEnumerable<string> Convert_to_strings(IEnumerable<int> ints) {
+			foreach(var item in ints) {
+				yield return Convert_to_string(item);
+			}
+		}
+
+		public string Convert_to_string(int item) {
+			return item.ToString();
+		}
+	}
+}
+
