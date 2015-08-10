3f2c087f3b1c510579c2858738d3fdacb6839688
Stefan Lieser <stefan@lieser-online.de>
2015-08-10 12:52:44 +0200

    Added code samples for dependencies

---
+++ source/csharp/implementation/implementation/dependencies/MultipleClasses.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace implementation
+{
+	public class MyClass
+	{
+		public int F(int x) {
+			var a = new A();
+			var b = new B();
+			var c = new C();
+
+			var p = a.Process(x);
+			var q = b.Process(p);
+			var z = c.Process(q);
+			return z;
+		}
+	}
+
+
+	public class A
+	{
+		public int Process(int x) {
+			return x + 1;
+		}
+	}
+
+
+	public class B
+	{
+		public int Process(int p) {
+			return q * 2;
+		}
+	}
+
+
+	public class C
+	{
+		public int Process(int q) {
+			return q - 1;
+		}
+	}
+}
+
---
+++ source/csharp/implementation/implementation/dependencies/OneClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace implementation
+{
+	public class OneClass
+	{
+		public int F(int x) {
+			var p = A(x);
+			var q = B(p);
+			var z = C(q);
+			return z;
+		}
+
+		public int A(int x) {
+			return x + 1;
+		}
+
+		public int B(int p) {
+			return q * 2;
+		}
+
+		public int C(int q) {
+			return q - 1;
+		}
+	}
+}
+
