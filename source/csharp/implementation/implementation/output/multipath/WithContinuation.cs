1bdce5a969f9fde11a4d5924730caa4b291b9c63
Stefan Lieser <stefan@lieser-online.de>
2015-08-11 14:38:57 +0200

    Rename

--- source/csharp/implementation/implementation/output/multipath/WithContinuation.cs
+++ source/csharp/implementation/implementation/output/multipath/WithContinuation.cs
@@ -4,10 +4,10 @@ namespace implementation.output.multipath
 {
 	public class MyClass
 	{
-		public void F(Action<string> First_Result, Action<int> Second_Result) {
+		public void F(Action<string> On_First_Result, Action<int> On_Second_Result) {
 			// ...
-			First_Result("my first result...");
-			Second_Result(42);
+			On_First_Result("my first result...");
+			On_Second_Result(42);
 		}
 	}
 }
