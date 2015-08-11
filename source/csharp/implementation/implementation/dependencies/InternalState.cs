5da6c6179a788fda8ffc2729afd27656916aa8fa
Stefan Lieser <stefan@lieser-online.de>
2015-08-11 09:56:41 +0200

    Added examples for implementing state

---
+++ source/csharp/implementation/implementation/dependencies/InternalState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace implementation
+{
+	public class InternalState
+	{
+		private int sum;
+
+		public void Sum(int? value, Action<int> onResult) {
+			if(value.HasValue) {
+				sum += value.Value;
+			} else {
+				onResult(sum);
+				sum = 0;
+			}
+		}
+	}
+}
+
---
+++ source/csharp/implementation/implementation/dependencies/StateInSameClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace implementation
+{
+	public class StateInSameClass
+	{
+		private int pageNo = 1;
+		private int lastPageNo = 42;
+
+		public int FirstPage() {
+			pageNo = 1;
+			return pageNo;
+		}
+
+		public int NextPage() {
+			if(pageNo < lastPageNo) {
+				pageNo++;
+			}
+			return pageNo;
+		}
+
+		public int PrevPage() {
+			if(pageNo > 1) {
+				pageNo--;
+			}
+			return pageNo;
+		}
+
+		public int LastPage() {
+			pageNo = lastPageNo;
+			return pageNo;
+		}
+	}
+}
+
