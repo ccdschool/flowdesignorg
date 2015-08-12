b7e8c72fd7716f24ac3f170e7109388c5373491e
Ralf Westphal <info@ralfw.de>
2015-08-12 13:13:00 +0200

    example code for logic

---
+++ source/csharp/logic/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace logic
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var name = args [0];
+			var greeting = Greetings.Greet (name);
+			Console.WriteLine (greeting);
+		}
+	}
+
+	class Greetings {
+		public static string Greet(string name) {
+			return "Hello, " + name + "!";
+		}
+	}
+}
