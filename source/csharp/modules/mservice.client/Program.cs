d0d2bc0d56b7aac0a62c85faad43ccc401180835
Ralf Westphal <info@ralfw.de>
2015-08-14 06:37:38 +0200

    added mservice

---
+++ source/csharp/modules/mservice.client/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using System.Net;
+using System.Web;
+
+namespace mservice.client
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var text = string.Join ("", args);
+			var url = string.Format ("http://localhost:1234?text={0}", HttpUtility.UrlEncode (text));
+
+			var result = "";
+			using(WebClient client = new WebClient()) {
+				result = client.DownloadString(url);
+			}
+
+			Console.WriteLine ("'{0}' {1}", text, result == "1" ? "is a palindrome" : "is not a palindrome");
+		}
+	}
+}
---
+++ source/csharp/modules/mservice.contract/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Nancy;
+using Nancy.Hosting.Self;
+
+using lib.palindromes;
+
+
+namespace mservice.contract
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			using (var host = new NancyHost(new Uri("http://localhost:1234")))
+			{
+				host.Start();
+				Console.WriteLine ("Palindrome checking service started...");
+				Console.ReadLine();
+			}
+		}
+	}
+
+
+	public class PalindromesService : NancyModule
+	{
+		public PalindromesService()
+		{
+			var palindromes = new lib.palindromes.Palindromes (new[]{ "stressed", "swap" });
+
+			Get["/"] = parameters => {
+				var text = this.Request.Query["text"];
+				Console.Write("  check: {0}", text);
+
+				var isPalindrome = palindromes.Check(text);
+				Console.WriteLine(" = {0}", isPalindrome);
+
+				return isPalindrome ? "1" : "0";
+			};
+		}
+	}
+}
